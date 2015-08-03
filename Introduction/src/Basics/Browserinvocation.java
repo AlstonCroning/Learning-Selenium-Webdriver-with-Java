93b8b1c0e01d7848d769c08093d61bc5b853f01b
Alston <and_croning@hotmail.com>
2015-08-03 17:38:40 -0400

    Invoking the Browser

---
+++ Introduction/src/Basics/Browserinvocation.java
@@ -0,0 +1,15 @@
+package Basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Browserinvocation 
+{
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+	
+		//using Firefox Driver class to execute and implement the methods present in Webdriver interface
+		WebDriver driver = new FirefoxDriver(); //invoking the Firefox Browser
+	}
+}
