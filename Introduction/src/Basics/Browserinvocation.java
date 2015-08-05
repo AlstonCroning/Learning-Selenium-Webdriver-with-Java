eab3ede98f9a028642cca4f001837562672daea7
Alston <and_croning@hotmail.com>
2015-08-05 00:04:48 -0400

    Basic Methods of WebDriver

--- Introduction/src/Basics/Browserinvocation.java
+++ Introduction/src/Basics/Browserinvocation.java
@@ -5,11 +5,17 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Browserinvocation 
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
 	
 		//using Firefox Driver class to execute and implement the methods present in Webdriver interface
 		WebDriver driver = new FirefoxDriver(); //invoking the Firefox Browser
+		driver.get("http://www.qaclickacademy.com/");//'get' is used to hit the URL in browser
+		System.out.println( driver.getTitle());//Page Title will be printed in the IDE output 
+		System.out.println( driver.getPageSource());//get the page source of the Home page
+		System.out.println(driver.getCurrentUrl()); //get the actual navigated URL
+		//driver.close(); //closes the browser (doesn't work for some reason)
+		driver.quit();//forced to quit the browser (works, but gives an error message since failed to close cleanly)
 	}
 }
