f5b7fa5b44570d4763ee55d0996ba211a395de0c
Alston <and_croning@hotmail.com>
2015-08-07 02:01:34 -0400

    Internet Explore Test Demonstrated in the same file

--- BasicChromeTest/src/ChromeTest.java
+++ BasicChromeTest/src/ChromeTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class ChromeTest {
 
@@ -7,12 +8,21 @@ public class ChromeTest {
 	{
 		// TODO Auto-generated method stub
 		
+		/*
+		//Test Chrome
 		//Runs the '.exe' file required for Chrome Browser (provides key="chrome driver" and value="path of the .exe")
 		//this gives permission for Chrome to run the tests in this application
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Dishan Croning\\Desktop\\Selenium Webdriver\\selenium-java-2.46.0\\selenium-2.46.0\\chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();//Invoking the  Chrome Browser
 		driver.get("https://www.google.ca");//hit the URL in the browser
+		 */		
+		
+		//Test Internet explore
+		System.setProperty("webdriver.ie.driver", "C:\\Users\\Dishan Croning\\Desktop\\Selenium Webdriver\\selenium-java-2.46.0\\selenium-2.46.0\\IEDriverServer.exe");
+		WebDriver driver = new InternetExplorerDriver();
+		driver.get("https://www.google.ca");//hit the URL in the browser
+		
 	}
 
 }
