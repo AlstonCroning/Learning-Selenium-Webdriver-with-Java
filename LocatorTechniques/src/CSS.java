47bd56db537c38f6ab27e2eea3f2e11e24285074
Alston <and_croning@hotmail.com>
2015-08-15 02:06:11 -0400

    Using Customized CSS path

--- LocatorTechniques/src/CSS.java
+++ LocatorTechniques/src/CSS.java
@@ -1,9 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class CSS {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://login.salesforce.com");
+		driver.findElement(By.cssSelector("[class='identity first']")).sendKeys("qweqweqe");
+		driver.findElement(By.cssSelector("input[id='password']")).sendKeys("kjsdfsf");
+		driver.findElement(By.xpath("//button[@id='Login']")).click();
+		
+		
+		
+		
 	}
 
 }
