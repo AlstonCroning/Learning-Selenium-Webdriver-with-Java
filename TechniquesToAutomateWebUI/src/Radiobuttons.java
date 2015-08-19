f43d38f063e0bf3b5a5b0391758466fb7caa2334
Alston <and_croning@hotmail.com>
2015-08-19 01:12:39 -0400

    Automation with Radio Buttons

--- TechniquesToAutomateWebUI/src/Radiobuttons.java
+++ TechniquesToAutomateWebUI/src/Radiobuttons.java
@@ -1,8 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Radiobuttons {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		WebDriver driver = new FirefoxDriver();//invoke the Firefox Browser
+		driver.get("http://www.echoecho.com/htmlforms10.htm");//hit the URL in the browser
+		driver.findElement(By.xpath("//input[@value='Milk']")).click();//click the radio button of the group 1 which has value 'Butter'
+
+		//print in the console how many radio buttons are in the group1 radio buttons
+		System.out.println( driver.findElements(By.xpath("//input[@name='group1']")).size());
 
 	}
 
