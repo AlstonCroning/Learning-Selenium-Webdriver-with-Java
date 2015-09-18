8bf73792c2b0a1357c9f88d65b7d32de8d77f100
Alston <and_croning@hotmail.com>
2015-09-17 23:20:28 -0400

    Preparing Files for 'Action()' Class Exmaple

---
+++ TechniquesToAutomateAdvancedWebUI/src/ActionsClassExample.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ActionsClassExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		
+		
+	}
+
+}
