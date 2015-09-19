54fd369723499b877e3cdf376694daca2ce178bc
Alston <and_croning@hotmail.com>
2015-09-19 01:23:01 -0400

    Preparing files for Handling Multiple Windows

---
+++ TechniquesToAutomateAdvancedWebUI/src/HandlingMultipleWindows.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class HandlingMultipleWindows {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://accounts.google.com/signup");
+		
+	}
+
+}
