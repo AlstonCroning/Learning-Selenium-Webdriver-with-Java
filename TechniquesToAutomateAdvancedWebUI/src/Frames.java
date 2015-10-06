2a6594d6e7f1761a59cc703b59c5c15fc675bc9b
Alston <and_croning@hotmail.com>
2015-10-05 23:37:54 -0400

    Handling 'Frames' simple example

--- TechniquesToAutomateAdvancedWebUI/src/Frames.java
+++ TechniquesToAutomateAdvancedWebUI/src/Frames.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -7,8 +8,11 @@ public class Frames {
 		// TODO Auto-generated method stub
 		
 		WebDriver driver = new FirefoxDriver();
-
+		driver.get("https://netbanking.hdfcbank.com/netbanking/");
+		System.out.println(driver.getPageSource());
 		
+		driver.switchTo().frame(1);
+		driver.findElement(By.cssSelector("input[class='input_password']")).sendKeys("15113161");
 	}
 
 }
