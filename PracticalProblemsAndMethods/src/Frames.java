558fba3af11549e4fedc92db669f0f1c2ba1d1db
Alston <and_croning@hotmail.com>
2015-11-09 01:43:21 -0500

    Handling Frames test case 1 complete

--- PracticalProblemsAndMethods/src/Frames.java
+++ PracticalProblemsAndMethods/src/Frames.java
@@ -17,17 +17,19 @@ public class Frames {
 
 		//get the count of iframes present in page
 		int FramesCount = driver.findElements(By.tagName("iframe")).size();//number if iframes present are 5
-	
-		int count = driver.findElements(By.xpath(".//*[@id='recaptcha-anchor']/div[5]")).size();//count is 0
-
-	
+		int count = 0;
 		
 		for(int i = 0; i < FramesCount; i++)
 		{
-			driver.switchTo().frame(1);
-			driver.findElement(By.xpath(".//*[@id='recaptcha-anchor']/div[5]")).click();
-			break;
+			driver.switchTo().frame(i);
+			count = driver.findElements(By.xpath(".//*[@id='recaptcha-anchor']/div[5]")).size();
 
+			if(count > 0)
+			{			
+				driver.findElement(By.xpath(".//*[@id='recaptcha-anchor']/div[5]")).click();
+				break;
+			}
+			driver.switchTo().defaultContent();
 		}
 		driver.switchTo().defaultContent();
 		
