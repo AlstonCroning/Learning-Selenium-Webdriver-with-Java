15a908009ebd3f9f7131c7566d0b3de6e51c028d
Alston <and_croning@hotmail.com>
2015-11-09 00:36:33 -0500

    Handling Multiple Frames to be continued..

--- PracticalProblemsAndMethods/src/Frames.java
+++ PracticalProblemsAndMethods/src/Frames.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -6,8 +7,33 @@ public class Frames {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		WebDriver driver = new FirefoxDriver();
+		/* Test Case: Handling Multiple Frames using Selenium
+		 * 1: click on the I'm not a Robot checkbox/element
+		 * 2: then click on the verify button
+		*/
+		
+		WebDriver driver = new FirefoxDriver();//invoke the Firefox browser
+		driver.get("https://fantasycricket.dream11.com");//hit the URL
 
+		//get the count of iframes present in page
+		int FramesCount = driver.findElements(By.tagName("iframe")).size();//number if iframes present are 5
+	
+		int count = driver.findElements(By.xpath(".//*[@id='recaptcha-anchor']/div[5]")).size();//count is 0
+
+	
+		
+		for(int i = 0; i < FramesCount; i++)
+		{
+			driver.switchTo().frame(1);
+			driver.findElement(By.xpath(".//*[@id='recaptcha-anchor']/div[5]")).click();
+			break;
+
+		}
+		driver.switchTo().defaultContent();
+		
+
+		
 	}
 
 }
+
